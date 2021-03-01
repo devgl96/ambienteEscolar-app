b5ccff2d988401158a805c9914fccd4fec45aab4
devgl96 <georgelucas.dev@gmail.com>
2021-03-01 17:05:54 -0300

    Styling ideas of Main Page

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -4,7 +4,17 @@ export default function Main() {
     return (
         <div className={styles.container}>
             <div className={styles.content}>
-                
+                <div className={styles.activities}>
+                    <div className={styles.card}>
+                        Carnê
+                    </div>
+                    <div className={styles.card}>
+                        Listas
+                    </div>
+                    <div className={styles.card}>
+                        Calendário
+                    </div>
+                </div>
             </div>
         </div>
     );
