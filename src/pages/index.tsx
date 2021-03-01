4ec19bdee4796b6d4ab20e8012a5880ff5ff6334
devgl96 <georgelucas.dev@gmail.com>
2021-03-01 16:47:43 -0300

    First Page of Application is Complete

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
 import Head from 'next/head'
+import React, { useState } from 'react';
 import styles from '../styles/pages/Home.module.css';
+import {Router, useRouter} from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+  const [schoolName, setSchoolName] = useState('');
+
+  function handleMain(e) {
+    e.preventDefault();
+    if(schoolName === 'Escola Crescer') {
+      router.push('/main');
+    } else {
+      alert("Preencha o campo com o nome da escola!");
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,9 +24,18 @@ export default function Home() {
       <div className={styles.modal}>
         <h1>Bem-Vindo ao Ambiente Escolar</h1>
         <form>
-          <p>Escolha o que deseja fazer</p>
+          <p>Digite o nome da escola</p>
           <section>
-            <button>Criar Carnê</button>
+            <input 
+              type="text"
+              placeholder="Ex.: Escola Crescer"
+              onChange={((e) => setSchoolName(e.target.value))}
+              required
+            />
+
+            <button onClick={handleMain}>
+                Acessar {schoolName}
+            </button>
           </section>
         </form>
       </div>
---
+++ src/pages/main.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/pages/Main.module.css';
+
+export default function Main() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.content}>
+                
+            </div>
+        </div>
+    );
+}
